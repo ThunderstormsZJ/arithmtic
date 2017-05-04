77895c8e0668f09b5b6cd0850bfb8acf6caac2eb
ThunderstormsZJ <z924680643@icloud.com>
2017-05-04 19:17:28 +0800

    add 0-1 knapsack problem second

--- src/arithmtic/ArithmticTest.java
+++ src/arithmtic/ArithmticTest.java
@@ -7,14 +7,12 @@ public class ArithmticTest {
 	public static void main(String[] args) {
 		Good[] goods = new Good[]{
 				new Good(3, 23), new Good(6, 103),
-				new Good(7, 99), new Good(4, 89),
+				new Good(7, 43), new Good(4, 89),
 				new Good(9, 67), new Good(2, 63),
 		};
 		
 		KnapsackProblem kProblem = new KnapsackProblem(30, goods);
-		kProblem.solveZoreOnePackFirst();
-		
-		kProblem.printBestValues();
+		kProblem.zoreOneProblemPrint();
 	}
 
 }
--- src/arithmtic/KnapsackProblem.java
+++ src/arithmtic/KnapsackProblem.java
@@ -5,37 +5,78 @@ import java.util.List;
 
 import model.Good;
 
-public class KnapsackProblem {
+abstract class BaseKnapsack {
+	// 背包最大承重
+	int totalWeight;
+
+	// 所有物品
+	Good[] goods;
+
+	// 物品的总数量
+	int goodSize = 0;
+
+	// 负无穷
+	double NEGATIVE = Double.NEGATIVE_INFINITY;
+
 	/*
 	 * 最优值矩阵 
 	 * 	对应相应物品在小于totalWeight的最优解 
 	 * 	行为所有物品,列为所有的weight
 	 * 
 	 */
-	private double[][] bestValues;
+	double[][] bestValues;
 
-	private double bestValue;
+	// 保存最优解的一维数组
+	double[] bestValuesArray;
 
-	private List<Good> bestGoods = new ArrayList<>();
+	double bestValue;
 
-	// 背包最大承重
-	private int totalWeight;
+	List<Good> bestGoods = new ArrayList<>();
 
-	// 所有物品
-	private Good[] goods;
-
-	// 物品的总数量
-	private int goodSize = 0;
-
-	// 负无穷
-	private double NEGATIVE = Double.NEGATIVE_INFINITY;
-
-	public KnapsackProblem(int totalWeight, Good[] goods) {
+	public BaseKnapsack(int totalWeight, Good[] goods) {
 		this.totalWeight = totalWeight;
 		this.goods = goods;
 		this.goodSize = goods.length;
+	}
+
+	void printBestValues() {
+		System.out.println("最优解矩阵:");
+		for (int i = 0; i < bestValues.length; i++) {
+			for (int j = 0; j < bestValues[i].length; j++) {
+				System.out.printf("%10.2f", bestValues[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	void printBestValue() {
+		System.out.printf("最优解:%.2f%n", bestValue);
+	}
+
+	void printBestGoods() {
+		System.out.println("最优解背包:");
+		for (int i = 0; i < bestGoods.size(); i++) {
+			System.out.println(bestGoods.get(i));
+		}
+	}
+	
+	void printBestValuesArray() {
+		System.out.println("最优解:");
+		for (int i = 0; i < bestValuesArray.length; i++) {
+			System.out.printf("%10.2f", bestValuesArray[i]);
+		}
+	}
+}
+
+class ZoreOneKnapsack extends BaseKnapsack {
+
+	public ZoreOneKnapsack(int totalWeight, Good[] goods) {
+		super(totalWeight, goods);
 		if (bestValues == null) {
 			bestValues = new double[this.goodSize + 1][this.totalWeight + 1];
+		}
+		if (bestValuesArray == null) {
+			bestValuesArray = new double[this.totalWeight + 1];
 		}
 	}
 
@@ -62,7 +103,10 @@ public class KnapsackProblem {
 				}
 			}
 		}
-
+		printFirstResult();
+	}
+	
+	private void printFirstResult() {
 		/*
 		 *  生成结果
 		 */
@@ -77,21 +121,47 @@ public class KnapsackProblem {
 			if (tempWeight <= 0)
 				break;
 		}
+		
+		printBestValues();
+		printBestValue();
+		printBestGoods();
 	}
 
-	public void printBestValues() {
-		System.out.println("最优解矩阵:");
-		for (int i = 0; i < bestValues.length; i++) {
-			for (int j = 0; j < bestValues[i].length; j++) {
-				System.out.printf("%10.2f", bestValues[i][j]);
+	/*
+	 * 	0-1背包问题（解法二）
+	 * 		问题：对解法一进行空间复杂度上进行优化，使用一维数组代替二维数组
+	 * 		思路:
+	 * 			1.bestValues[i][j]的值通过一维数组保存,也就意味着第i次循环可以得到bestValuesArray[j]
+	 * 			2.bestValues[i][j]主要是由{bestValue[i-1][j], bestValue[i-1][j-gWeight]}推出
+	 * 			3.因此采用逆序的方式遍历totalWeight能够保证计算bestValuesArray[j]时事先已经得到bestValuesArray[j-gWeight]的值
+	 * 		初始化问题:
+	 * 			1.若需要得到恰好装满背包的最大物品价值,则需要将bestValuesArray[0]初始化为0,其他的初始化为负无穷
+	 * 			2.若只是求出得到背包可以装下的最大的价值,则只需要将bestValuesArray全部初始化为0
+	 */
+	public void solveZoreOnePackSecond() {
+		for (int i = 0; i < goodSize; i++) {
+			double gValue = goods[i].getValue();
+			int gWeight = goods[i].getWeight();
+			for (int j = totalWeight; j >= gWeight; j--) {
+				bestValuesArray[j] = Math.max(bestValuesArray[j], bestValuesArray[j - gWeight] + gValue);
 			}
-			System.out.println();
 		}
-		System.out.printf("最优解:%.2f%n", bestValue);
-		System.out.println("最优解背包:");
-		for (int i = 0; i < bestGoods.size(); i++) {
-			System.out.println(bestGoods.get(i));
+		printBestValuesArray();
+	}
+}
+
+public class KnapsackProblem {
+	private ZoreOneKnapsack zoreOneKnapsack = null;
+
+	public KnapsackProblem(int totalWeight, Good[] goods) {
+		if (zoreOneKnapsack == null) {
+			zoreOneKnapsack = new ZoreOneKnapsack(totalWeight, goods);
 		}
+	}
+
+	public void zoreOneProblemPrint() {
+		zoreOneKnapsack.solveZoreOnePackFirst();
+		zoreOneKnapsack.solveZoreOnePackSecond();
 	}
 
 }
